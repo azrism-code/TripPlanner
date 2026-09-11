a3bb3889acac5589e0e3c557b09f4618b5724dfb
azrism-code <azrism@gmail.com>
2026-09-11 07:31:15 -0700

    Switch app entry to v0.5 interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
-import App from './App.jsx'
+import App from './AppV05.jsx'
 import './styles.css'
 import './v04.css'
+import './v05.css'
 
 registerSW({ immediate: true })
 
