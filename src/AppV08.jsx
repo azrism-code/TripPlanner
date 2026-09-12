5baf5658cfb453158242396ece54129a6e3ee3cc
azrism-code <azrism@gmail.com>
2026-09-12 20:13:12 +0300

    Move Trip AI access to the topbar

--- src/AppV08.jsx
+++ src/AppV08.jsx
@@ -1,11 +1,13 @@
 import LegacyApp from './AppV07.jsx'
 import SmartDocumentLayer from './SmartDocumentLayer.jsx'
+import TopbarAiLayer from './TopbarAiLayer.jsx'
 
 export default function AppV08() {
   return (
     <>
       <LegacyApp />
       <SmartDocumentLayer />
+      <TopbarAiLayer />
     </>
   )
 }
