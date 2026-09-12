37a482072e146647de544270c4a3dab045ee05cc
azrism-code <azrism@gmail.com>
2026-09-12 06:25:14 -0700

    Add v0.7 app wrapper

---
+++ src/AppV08.jsx
@@ -0,0 +1,11 @@
+import LegacyApp from './AppV07.jsx'
+import SmartDocumentLayer from './SmartDocumentLayer.jsx'
+
+export default function AppV08() {
+  return (
+    <>
+      <LegacyApp />
+      <SmartDocumentLayer />
+    </>
+  )
+}
