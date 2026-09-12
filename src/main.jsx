48d4b3d6e80ccc86596b41db0ac3e2659f5ecd12
azrism-code <azrism@gmail.com>
2026-09-12 06:25:56 -0700

    Wire v0.7 smart document UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
-import App from './AppV07.jsx'
+import App from './AppV08.jsx'
 import './styles.css'
 import './v04.css'
 import './v05.css'
 import './v07.css'
+import './v08.css'
 
 registerSW({ immediate: true })
 
